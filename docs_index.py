2f1516ec537fc5a351e419457a306a3c72146e35
panasenco <panasenco@users.noreply.github.com>
2021-01-26 16:31:20 -0800

    Figured out the HTML and CSS to render a single directory

---
+++ docs_index.py
@@ -0,0 +1,11 @@
+﻿from jinja2 import Environment, FileSystemLoader
+
+def contents(path: str):
+    with open(path, 'r', encoding='utf-8') as f:
+        return f.read()
+
+if __name__ == '__main__':
+    env = Environment(loader = FileSystemLoader(searchpath="."))
+    template = env.get_template('docs-index.html')
+    with open('index.html', 'w', encoding='utf-8') as f:
+        f.write(template.render(css=contents('styles/_site/ui/css/styles.css')))
