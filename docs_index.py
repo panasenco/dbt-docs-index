7d0c745418b76d8e280a25172e1ebe5113b97e79
panasenco <panasenco@users.noreply.github.com>
2021-01-26 17:02:32 -0800

    Can now render a test tree

--- docs_index.py
+++ docs_index.py
@@ -1,4 +1,15 @@
-﻿from jinja2 import Environment, FileSystemLoader
+﻿from dataclasses import dataclass, field
+from typing import List
+
+from jinja2 import Environment, FileSystemLoader
+
+@dataclass
+class Node:
+    name: str
+    children: List['Node'] = field(default_factory=lambda: [])
+    href: str = ''
+
+TEST_TREE = [Node('p1', [Node('p11', [Node('c111', href='c111')])]), Node('p2', [Node('c21', href='c21')]), Node('c2', href='c2')]
 
 def contents(path: str):
     with open(path, 'r', encoding='utf-8') as f:
@@ -8,4 +19,9 @@ if __name__ == '__main__':
     env = Environment(loader = FileSystemLoader(searchpath="."))
     template = env.get_template('docs-index.html')
     with open('index.html', 'w', encoding='utf-8') as f:
-        f.write(template.render(css=contents('styles/_site/ui/css/styles.css')))
+        f.write(
+            template.render(
+                css=contents('styles/_site/ui/css/styles.css'),
+                tree=TEST_TREE,
+                )
+            )
