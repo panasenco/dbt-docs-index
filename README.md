3fe3fae3cb7b62e1ba8d67d8330592b3525ca4d3
panasenco <panasenco@users.noreply.github.com>
2021-01-27 17:20:57 -0800

    Finally using os.walk to generate a real directory dict!

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # dbt-docs-index
 Generates an index of multiple dbt documentation sites
+
+## Usage
+
+```
+python docs_index.py [ROOT DIRECTORY]
+```
+
+generates a file index.html in the root directory with a directory listing of your dbt projects:
+
+![menu of dbt docs sites](/etc/screenshot.png?raw=true "dbt-docs-index screenshot")
--- docs_index.py
+++ docs_index.py
@@ -1,27 +1,44 @@
-﻿from dataclasses import dataclass, field
-from typing import List
+﻿import os
+from pathlib import PurePath, PurePosixPath
+import sys
 
+import dpath.util
 from jinja2 import Environment, FileSystemLoader
 
-@dataclass
-class Node:
-    name: str
-    children: List['Node'] = field(default_factory=lambda: [])
-    href: str = ''
-
-TEST_TREE = [Node('p1', [Node('p11', [Node('c111', href='c111')])]), Node('p2', [Node('c21', href='c21')]), Node('c2', href='c2')]
+TEST_TREE = {
+    'Dimensional Models': {
+        'Model Category 1': {
+            'Model Site 1-1': 'm1-1',
+        }
+    },
+    'Table Discovery (Read-Only)': {
+        'Source DB 1': 's1',
+        'Source DB 2': 's2',
+    },
+    'Top-Level Model': 'mt',
+}
 
 def contents(path: str):
     with open(path, 'r', encoding='utf-8') as f:
         return f.read()
 
 if __name__ == '__main__':
+    root = PurePath(sys.argv[1] if len(sys.argv) > 1 else '.')
+    tree = {}
+    for dirpath, dirnames, filenames in os.walk(root):
+        if 'dbt_project.yml' in filenames and 'target' in dirnames:
+            relative_path = PurePath(dirpath).relative_to(root)
+            url = PurePosixPath(relative_path, 'target', 'index.html')
+            print(f'Found dbt project: {url}')
+            dpath.util.new(tree, relative_path.parts, str(url))
+            del dirnames[:] # Don't look for dbt projects inside dbt projects
+    print(tree)
     env = Environment(loader = FileSystemLoader(searchpath="."))
     template = env.get_template('docs-index.html')
-    with open('index.html', 'w', encoding='utf-8') as f:
+    with open(PurePath(root, 'index.html'), 'w', encoding='utf-8') as f:
         f.write(
             template.render(
                 css=contents('styles/_site/ui/css/styles.css'),
-                tree=TEST_TREE,
+                tree=tree,
                 )
             )
